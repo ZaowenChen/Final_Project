dac34f7ae97edb339f140c3336874ea749cd9f5a
will7i1am3 <whaung.2003@gmail.com>
2024-04-14 21:29:45 -0400

    highlighting portion of homepage

--- app/src/main/java/com/example/finalproject/HomeActivity.java
+++ app/src/main/java/com/example/finalproject/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.finalproject;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -8,6 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -19,78 +22,41 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        db = new Database (this, "database");
+        db = new Database(this, "database");
         // Received intent and get sharedpreferences
         Intent received_intent = getIntent();
         String username = received_intent.getStringExtra("Username");
 
         String name = db.getName(username);
 
-        TextView header = findViewById(R.id.header);
-        header.setText("Welcome " + name + "!");
+        TextView global = findViewById(R.id.globalposts);
+        TextView friends_post = findViewById(R.id.friendposts);
 
-//
-//        // For diary
-//        diary.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d("Home Activity", "diary: was pressed ");
-//                Intent intent = new Intent(getApplicationContext(), DailyDiaryActivity.class);
-//                intent.putExtra("Username", username);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        // For viewnotes
-//        notes.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d("Home Activity", "notes: was pressed ");
-//                Intent intent = new Intent(getApplicationContext(), ViewNotesActivity.class);
-//                intent.putExtra("Username", username);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        // For tracker
-//        mood.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d("Home Activity", "tracker: was pressed ");
-//                Intent intent = new Intent(getApplicationContext(), TrackerActivity.class);
-//                intent.putExtra("Username", username);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        // For helpful links
-//        links.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d("Home Activity", "links: was pressed ");
-//                Intent intent = new Intent(getApplicationContext(), HelpfulLinksActivity.class);
-//                intent.putExtra("Username", username);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        // For logout
-//        logout.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d("Home Activity", "logout: was pressed ");
-//                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//
-//    }
-//
-//    @Override
-//    public void onBackPressed() {
-//        // Disable back pressed
-//        Log.d("Home Activity", "onBackPressed: was pressed ");
+        global.setBackgroundColor(Color.parseColor("@color/columbia_blue"));
+
+        global.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                global.setBackgroundColor(Color.parseColor("@color/columbia_blue"));
+                friends_post.setBackgroundColor(Color.parseColor("@color/white"));
+            }
+        });
+
+        friends_post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                friends_post.setBackgroundColor(Color.parseColor("@color/columbia_blue"));
+                global.setBackgroundColor(Color.parseColor("@color/white"));
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Disable back pressed
+        Log.d("Home Activity", "onBackPressed: was pressed ");
 
     }
 
